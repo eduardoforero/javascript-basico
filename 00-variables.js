8559d25c2217e5fa02dc4040984c43436785e01e
Eduardo Forero <eduardo.forero.r@gmail.com>
2024-08-23 11:35:03 -0500

    Agrego el primer archivo sobre variables

---
+++ 00-variables.js
@@ -0,0 +1,26 @@
+// var
+
+/* Puede redeclararse y reasignarse 
+   tiene solo scope global o de función
+   permite un hoisting de iniciación a undefined
+*/
+
+var x = ""
+
+// let
+
+/* No pueden redeclararse, pero sí reasignarse
+   Tienen scope global, de función o de bloque
+   en hoisting no se inicializa a ningún valor
+*/
+
+let y = ""
+
+// const
+
+/* No puede redeclararse ni reasignarse
+   tiene scope global, de función o de bloque
+   en hoisting no se inicializa a ningún valor
+*/
+
+const z = ""
