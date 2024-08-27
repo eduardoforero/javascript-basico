79109c1d2db6ac319f579fc3358631b21e242cbb
Eduardo Forero <eduardo.forero.r@gmail.com>
2024-08-27 18:58:51 -0500

    Actualización del archivo sobre Tipos de Datos

--- 01-tipos-de-datos.js
+++ 01-tipos-de-datos.js
@@ -1,56 +1,42 @@
 // string
 
 /* Cadena de texto: 
-   Se utiliza para representar datos textuales
-*/
-
+   Se utiliza para representar datos textuales */
 let myName = "Eduardo Forero"
 
 // number
 
 /* Número: Además de representar números de punto flotante
-   tiene tres valores simbólicos: +Infinity, -Infinity y NaN ("Not a Number")
-*/
-
+   tiene tres valores simbólicos: +Infinity, -Infinity y NaN ("Not a Number") */
 let myAge = 18
 
 // boolean
 
 /* Verdadero o Falso: Representa una entidad lógica 
-   puede tener dos valores: true y false
-*/
-
+   puede tener dos valores: true y false */
 let isHuman = true
 
 // undefined
 
 /* Indefinido: Una variable a la que 
-   No se le ha asignado un valor  
-*/
-
+   No se le ha asignado un valor */
 let unDefinedValue
 
 // null
 
 /* Inexistente o no válido
-   tiene exactamente un valor: null
-*/
-
+   tiene exactamente un valor: null */
 let nullValue = null
 
 // symbol
 
 /* Símbolo: Valor primitivo único e inmutable 
-   se puede utilizar como clave de una propiedad de objeto
-*/
-
+   se puede utilizar como clave de una propiedad de objeto */
 let mySymbol = Symbol()
 
 // BigInt
 
 /* Puede representar números enteros con precisión arbitraria
    se puede almacenar y operar de forma segura en números enteros grandes 
-   incluso más allá del límite seguro de enteros para (number).
-*/
-
+   incluso más allá del límite seguro de enteros para (number) */
 let myBigInt = BigInt(888484995695960596506)
